f5e73c9f57b3a216750f6a63843e56e9bf7b4be1
Afaq <afaqnaveed@gmail.com>
2025-05-29 22:32:04 +0500

    Created two templates one content.php and other is content-none.php. Also used WordPress get_search_form to display the search form also used wp_kses function

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ get_header();
   <div id="primary">
     <main id="main" class="site-main mt-5" role="main">
       <?php
-      if ( have_posts() ) {
+      if ( have_posts() ) :
         ?>
         <div class="container">
         <?php
@@ -31,21 +31,16 @@ get_header();
           
             //Start the loop.
             while ( have_posts() ) : the_post();
-                ?>
-
-              <div class="col-lg-4 col-md-6 col-sm-12">
-              
-              <h3><?php the_title(); ?></h3>
-              <div><?php the_excerpt(); ?></div>
-              </div>
-
-              <?php
+              get_template_part( 'template-parts/content');
             endwhile;
             ?>
           </div>
         </div>
         <?php
-      }
+
+      else : get_template_part( 'template-parts/content-none' );
+
+      endif;
       ?>
     </main>
   </div>
---
+++ template-parts/content.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Content template
+ * 
+ * @package Aquila
+ */
+?>
+
+<div class="col-lg-4 col-md-6 col-sm-12">             
+<h3><?php the_title(); ?></h3>
+<div><?php the_excerpt(); ?></div>
+</div>
